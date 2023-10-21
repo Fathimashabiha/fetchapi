840ebab907f1f7aafd67f7b26f72e1ba6e6ddd38
Fathimashabiha <fathimashabiha1905@gmail.com>
2023-10-21 22:03:07 +0530

    api fetch added

---
+++ script.js
@@ -0,0 +1,23 @@
+function fetchData() {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(response => response.json())
+        .then(data => {
+            const main = document.querySelector('main');
+            data.forEach(value => {
+                const valueElement = document.createElement('div');
+                valueElement.classList.add('value');
+                valueElement.innerHTML = `
+                    <h3>Name: ${value.name}</h3>
+                    <h4>Email: ${value.email}</h4>
+                    <p>${value.body}</p>
+                `;
+                main.appendChild(valueElement);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+
+fetchData();
